54412f45a37c8876c84ddd86b3091c08e92413c7
Alvin James T. Delos Santos <a.jamesdelossantos@gmail.com>
2025-02-12 15:03:36 -0600

    chore: update configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/PokeAPI/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
