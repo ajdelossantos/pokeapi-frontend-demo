8ab36344518b9aac776296d597fac29f9a47fdb4
Alvin James T. Delos Santos <a.jamesdelossantos@gmail.com>
2025-02-11 21:14:14 -0600

    chore: migrate /app

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h2>PokeAPI Demo</h2>
+        <p>
+          A demo of NextJS, OpenAPI TS, and Tanstack Query using the PokeAPI.
+        </p>
+      </main>
+      <footer>
+        <p>Footer goes here</p>
+      </footer>
+    </div>
+  );
+}
